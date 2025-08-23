190305a1c33b3a0dba24779c1ab56e3df7c9f1c7
Felipe <felipebs10@gmail.com>
2025-08-23 19:08:35 -0300

    aula 08

---
+++ aula08/domain/lancamento.js
@@ -0,0 +1,29 @@
+class Lancamento {
+            
+            constructor (categoria, tipo, valor){
+                if (tipo !== "receita" && tipo !== "despesa"){
+                    throw new Error ("Lançamento Inválido: Tipo deve ser receita ou despesa")
+                }
+
+                if (valor <= 0){
+                    throw new Error ("Lançamento Inválido: Valor deve ser maior que zero")
+                }
+
+                if (categoria === ""){
+                    throw new Error ("Lançamento Inválido: Categoria é obrigatória")
+                }
+                this.categoria = categoria;
+                this.tipo = tipo;
+                this.valor = valor;
+            }
+            getValorString(){
+             /*   if (this.tipo === "despesa"){
+                    return this.valor * -1;
+                }else {
+                    return this.valor;
+                }*/
+                //operador ternário - Simplificnando o if acima, direto no return;
+                return (this.tipo === "despesa") ? this.valor * -1 : this.valor;
+                
+            }
+        }
---
+++ aula08/main.js
@@ -0,0 +1,5 @@
+        const tela = new Tela();
+        tela.renderizar();
+
+            
+           
