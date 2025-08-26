6db88eec1f3a61e7f1fba4fcead24c769451f5d2
Felipe <felipebs10@gmail.com>
2025-08-25 22:42:40 -0300

    aula 09 primeira parte

---
+++ aula09/client/domain/ano.js
@@ -0,0 +1,30 @@
+class Ano {
+
+    constructor () {
+        this.meses = [];
+    }
+
+    adicionarMes (mes) {
+        this.meses.push(mes);
+    }
+
+    adicionarLancamento(nomeDoMes, lancamento){
+        for (const mes of this.meses) {
+            if (mes.nome === nomeDoMes){
+                mes.adicionarLancamento(lancamento);
+                break; //interrompe o laço
+            }
+        }
+    }
+
+    calcularSaldo () {
+        let saldoInicial = 0;
+        for (const mes of this.meses){
+            mes.saldoInicial = saldoInicial;
+            mes.calcularSaldo();
+            saldoInicial = mes.totalizador.saldo;
+
+        }
+    }
+
+}
